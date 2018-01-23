5096d2324842504786793f177b4abcd310808ca5
Mosima <mosimagodfrey@gmail.com>
2018-01-23 16:42:26 +0200

    discount

--- src/cards/ViewCard.js
+++ src/cards/ViewCard.js
@@ -1,8 +1,9 @@
 import React,{ Component } from 'react'
-import { Grid, Image } from 'semantic-ui-react'
+import { Grid, Image, Container, Item, Icon, Form, TextArea, Rating, Button, Divider } from 'semantic-ui-react'
 import ProductCard from './ProductCard'
 import { connect } from "react-redux";
-
+import Products from '../products/ProductsCarousel'
+import pic from '../images/images/01.jpg'
 
 class ViewProduct extends Component{
     constructor(props)
@@ -13,42 +14,54 @@ class ViewProduct extends Component{
         }
     }
     render(){
-       
+       // <ProductCard productId={this.props.match.params.filter}/>
         return(
             <div>
-                <Grid centered columns={2}>
-                    <Grid.Column>
-                        <ProductCard productId={this.props.match.params.filter}/>
-                    </Grid.Column>
-                    <Grid.Row centered columns={4}>
-                                    <Grid.Column>
-                                        <Image src='/assets/images/wireframe/image.png' />
-                                    </Grid.Column>
-                                    <Grid.Column>
-                                        <Image src='/assets/images/wireframe/image.png' />
-                                    </Grid.Column>
-                                    <Grid.Column>
-                                        <Image src='/assets/images/wireframe/image.png' />
-                                    </Grid.Column>
-                                    </Grid.Row>
+                <Container>
                     
-                                    <Grid.Row centered columns={4}>
-                                    <Grid.Column>
-                                        <Image src='/assets/images/wireframe/image.png' />
-                                    </Grid.Column>
-                                    <Grid.Column>
-                                        <Image src='/assets/images/wireframe/image.png' />
-                                    </Grid.Column>
-                                    <Grid.Column>
-                                        <Image src='/assets/images/wireframe/image.png' />
-                                    </Grid.Column>
-                                    <Grid.Column>
-                                        <Image src='/assets/images/wireframe/image.png' />
-                                    </Grid.Column>
-                                    </Grid.Row>
- 
-            
+                  <Grid>
+                    <Grid.Column width={10}>
+                    <Image src={pic} />
+                    </Grid.Column>
+                    <Grid.Column width={6}>
+                    <Item>
+                         <Item.Content>
+                            <Item.Header as='h2'>Computer</Item.Header>
+                            <Item.Description as='h4' style={{color:"red"}}>
+                                R5000
+                            </Item.Description>
+                            <Item.Description as='h6' style={{textDecoration:'line-through'}}>
+                                was R5500
+                            </Item.Description >
+                            <Item.Description as='h6' >
+                                Store : GAME Mirand
+                            </Item.Description>
+                            <Item.Extra>
+                            <Icon color='green' name='check' /> 121 Votes
+                            </Item.Extra>
+                         </Item.Content>
+                     </Item>
+                    </Grid.Column>
                 </Grid>
+                <Grid>
+                    <Grid.Column width={10}>
+                        <Form>
+                            <Form.Field>
+                                <TextArea rows={2} placeholder='What do you think about this product?' />
+                            </Form.Field>
+                            <Button basic color='red' type='submit'>Submit</Button>   
+                        </Form>
+                    </Grid.Column>
+                    <Grid.Column width={6}>
+                        <Button color='red' type='submit'>11 Km away</Button>
+                    </Grid.Column>
+                    <Divider horizontal>Products That in same category</Divider>
+                    <Grid.Column width={16}>
+                        <Products/>
+                    </Grid.Column>
+
+                </Grid>
+                </Container>
             </div>
         )
     }
